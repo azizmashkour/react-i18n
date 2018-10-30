5c1f464d956dbb7ac191b0e9dd1c593b01f839d9
Mashkour <azizmashkour@gmail.com>
2018-10-30 14:23:10 +0100

    feat: add language select box and pass all languages as params

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { withNamespaces } from 'react-i18next';
+import Select from 'react-select';
+import { options } from './config/options';
+import './App.css';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lang: options[0],
+    };
+  }
+
+  changeLang = (lang) => {
+    const { i18n } = this.props;
+    const { value } = lang;
+    this.setState({
+      lang,
+    });
+    i18n.changeLanguage(value);
+  };
+
+  render() {
+    const { lang } = this.state;
+    const { t } = this.props;
+
+    return (
+      <div className="App-Root">
+        <Select
+          defaultValue={options[0]}
+          options={options}
+          value={lang}
+          onChange={this.changeLang}
+          className="App-Select"
+        />
+
+        <h3 className="text-center pt-5">{t('Welcome to React Translation')}</h3>
+      </div>
+    );
+  }
+}
+
+export default withNamespaces('translations')(App);
