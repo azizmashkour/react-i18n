30be4573c034a0397f5b47927eaa850f00c16d58
Mashkour <azizmashkour@gmail.com>
2018-10-30 14:24:28 +0100

    feat: create language selection config file for react-select

---
+++ src/config/options.js
@@ -0,0 +1,10 @@
+// todo remove suppression on multiple exports
+// eslint-disable-next-line import/prefer-default-export
+export const options =
+[
+  { value: 'en', label: 'English' },
+  { value: 'fr', label: 'French' },
+  { value: 'de', label: 'Germany' },
+  { value: 'es', label: 'Spanish' },
+  { value: 'it', label: 'Italian' },
+];
