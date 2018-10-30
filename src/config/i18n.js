77ff714d1f7102e3bb713d9300eaf6dd576b0e6c
Mashkour <azizmashkour@gmail.com>
2018-10-30 14:23:42 +0100

    feat: create i18n config file and import all languages files

---
+++ src/config/i18n.js
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { reactI18nextModule } from 'react-i18next';
+import en from '../locale/en';
+import fr from '../locale/fr';
+import it from '../locale/it';
+import de from '../locale/de';
+import es from '../locale/es';
+
+i18n
+  .use(reactI18nextModule)
+  .init({
+    resources: {
+      en,
+      fr,
+      it,
+      de,
+      es,
+    },
+    fallbackLng: 'en',
+    debug: true,
+    ns: ['translations'],
+    defaultNS: 'translations',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ',',
+    },
+    react: {
+      wait: true,
+    },
+  });
+export default i18n;
